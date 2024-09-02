4f3e3d130614f1bd9ffd8dfa3e5ff013aa2276df
Kmore1515 <kylebradleymorris@usf.edu>
2024-09-01 22:30:55 -0400

    Task 2 - Product Inventory Management

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ let employeename = "Billy Bob"
 const employeeID = 12345
 var isactive = true
 console.log(employeename, employeeID, isactive)
+
+let products = ["Apples", "Oranges", "Pears"]
+const productdetails = { name: "Apples", price: 2.15, instock: true}
+const productdetails = { name: "Oranges", price 3.00, instock: true}
+const productdetails = { name: "Pears", price: 2.75, instock: true}
+console.log(products, productdetails)
