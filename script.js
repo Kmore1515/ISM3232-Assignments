6b19e4d2fea549abcf9737a4e51a31c663928592
Kmore1515 <kylebradleymorris@usf.edu>
2024-09-01 22:47:36 -0400

    Task 5 - Access Control System

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
-let employeename = "Billy Bob"
-const employeeID = 12345
-var isactive = true
-console.log(employeename, employeeID, isactive)
+let employeename = "Billy Bob";
+const employeeID = 12345;
+var isactive = true;
+console.log(employeename, employeeID, isactive);
 
-let products = ["Apples", "Oranges", "Pears"]
-const productdetails = { name: "Apples", price: 2.15, instock: true}
-const productdetails = { name: "Oranges", price 3.00, instock: true}
-const productdetails = { name: "Pears", price: 2.75, instock: true}
-console.log(products, productdetails)
+let products = ["Apples", "Oranges", "Pears"];
+const productdetails = { name: "Apples", price: 2.15, instock: true};
+const productdetails = { name: "Oranges", price 3.00, instock: true};
+const productdetails = { name: "Pears", price: 2.75, instock: true};
+console.log(products, productdetails);
 
-let accountbalance = 25,000 
-console.log(accountbalance, "Original Balance")
-accountbalance += 200
-console.log(accountbalance, "Balance after Deposit")
-accountbalance -= 2,500
-console.log(accountbalance, "Balance after Withdrawl")
+let accountbalance = 25,000 ;
+console.log(accountbalance, "Original Balance");
+accountbalance += 200;
+console.log(accountbalance, "Balance after Deposit");
+accountbalance -= 2,500;
+console.log(accountbalance, "Balance after Withdrawl");
 
-let employeescore1 = 86
-let employeescore2 = 91
-console.log(employeescore1 < employeescore2)
+let employeescore1 = 86;
+let employeescore2 = 91;
+console.log(employeescore1 < employeescore2);
 
+let haskeycard = true;
+let haspermission = true;
+
+let canaccessroom1 = haskeycard && haspermission;
+let canaccessroom2 = haskeycard || haspermission;
+
+let canaccessroom1 = !cannotaccessroom1;
+console.log(haskeycard, haspermission, canaccessroom1, canaccessroom2,);
