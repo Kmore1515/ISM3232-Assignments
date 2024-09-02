6251a05f85326625b5c09ef01efbd1abe75e1fc7
Kmore1515 <kylebradleymorris@usf.edu>
2024-09-01 22:37:27 -0400

    Task 3 - Financial Calculations

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ const productdetails = { name: "Apples", price: 2.15, instock: true}
 const productdetails = { name: "Oranges", price 3.00, instock: true}
 const productdetails = { name: "Pears", price: 2.75, instock: true}
 console.log(products, productdetails)
+
+let accountbalance = 25,000 
+console.log(accountbalance, "Original Balance")
+accountbalance += 200
+console.log(accountbalance, "Balance after Deposit")
+accountbalance -= 2,500
+console.log(accountbalance, "Balance after Withdrawl")
+
